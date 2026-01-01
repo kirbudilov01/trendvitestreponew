f77a2f42446420809db43b0443a23e2f0b27315a
kirbudilov01 <budilov02ke@gmail.com>
2026-01-01 05:48:12 +0300

    Delete collector directory

--- collector/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class CeleryConfig:
-    broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
-    result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
-
-settings = CeleryConfig()
